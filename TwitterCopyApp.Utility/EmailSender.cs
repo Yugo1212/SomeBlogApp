274e4a25343305d372bf002bca39811c1db41582
Adam Kaszyński <73426421+Yugo1212@users.noreply.github.com>
2021-11-30 00:24:59 +0100

    Add files via upload

---
+++ TwitterCopyApp.Utility/EmailSender.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterCopyApp.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailOptions _emailOptions;
+
+        public EmailSender(IOptions<EmailOptions> options)
+        {
+            _emailOptions = options.Value;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(_emailOptions.SendGridKey, subject, htmlMessage, email);
+        }
+
+        private Task Execute(string sendGridKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(sendGridKey);
+            var from = new EmailAddress("adam_kaszynski@tlen.pl", "Twitter copy app");
+            var to = new EmailAddress(email, "End User");
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", message);
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
---
+++ TwitterCopyApp.Utility/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterCopyApp.Utility
+{
+    public class Roles
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Moderator = "Morderator";
+        public const string Role_User = "User";
+    }
+}
